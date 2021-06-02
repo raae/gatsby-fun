46999e3ed6326604a75d6615ced366771797a008
Benedicte Raae <raae@raae.codes>
2021-06-02 09:36:18 +0200

    Remove plugins added by gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,14 +13,12 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
       },
     },
-    "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
